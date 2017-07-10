c192fa7c74ce850adc1f33fa9fa438a2f1e292d8
Girls Who Code 09 <gwc09@ad.cs.fiu.edu>
2017-07-10 15:10:39 -0400

    Changes

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ var main =  function(){
 
 
 $(document).ready(main);
+
+	$(".navbara,footer a[href='#myPage']").on('click',function(event){
+		
+		//hash value overrides default behavior
+		if (this.hash !== "") {
+			
+		event.preventDefault();
+		
+		var hash = this.hash;
+		
+		$('html, body').animate({
+			scrollTop:$(hash).offset().top
+		}, 900, function(){
+			
+			window.location.hash = hash;
+		});
+	} //End if
+	});
+})
